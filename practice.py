846422e05fc0a27a2dfb0a3b45b1d7587de95897
hkwak60 <hkwak60@gmail.com>
2024-03-30 20:23:08 +0900

    Create practice.py

---
+++ practice.py
@@ -0,0 +1,18 @@
+import dns.query
+import dns.resolver
+
+# 질의할 도메인 주소 입력
+domain = 'google.com'
+
+# 사용할 DNS Server 주소 입력
+dns_server = '8.8.8.8'
+
+# 도메인의 A record에 대한 DNS 쿼리 생성
+query = dns.message.make_query(domain, 'A')
+print(query)
+
+# DNS 서버로 쿼리 전송 및 응답 받기
+response = dns.query.udp(query, dns_server)
+
+# 응답 출력
+#print(response)
